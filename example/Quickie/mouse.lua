4b8a6ef69f837593458ec377ceb2fa27af78c3d3
WetDesertRock <wetdesertrock@gmail.com>
2018-10-25 06:48:17 -0600

    Update Quickie to 11.1
    This isn't a complete update, just updating the features this example 
    uses.

--- example/Quickie/mouse.lua
+++ example/Quickie/mouse.lua
@@ -63,7 +63,7 @@ local function beginFrame()
 	x,y = _M.getMousePosition()
 	downLast = down
 	down = false
-	for _,btn in ipairs{'l', 'm', 'r'} do
+	for _,btn in ipairs{1, 3, 2} do
 		down = down or (love.mouse.isDown(btn) and btn)
 	end
 end
